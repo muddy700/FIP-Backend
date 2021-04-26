6d06c4b8c3e2c6514d4175319cb4921fd5cc9dcd
muddy700 <mohamedmfaume700@gmail.com>
2021-04-26 14:42:04 +0300

    Completed Backend Version 1

--- fip_project/student_profile_app/admin.py
+++ fip_project/student_profile_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import StudentProfile, StudentProfession
+from .models import StudentProfile, StudentProfession, FieldReport
 
 admin.site.register(StudentProfile)
+admin.site.register(FieldReport)
 admin.site.register(StudentProfession)
