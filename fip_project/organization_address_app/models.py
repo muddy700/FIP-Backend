ae402766a6cc0bf60a49f8dc96e6ce7d63f2f387
Mohamed Mfaume <mohamedmfaume700@gmail.com>
2021-05-27 12:16:21 +0300

    Added Progress V2

--- fip_project/organization_address_app/models.py
+++ fip_project/organization_address_app/models.py
@@ -5,6 +5,7 @@ from profession_app.models import Profession
 class OrganizationProfile(models.Model):
     organization_id = models.OneToOneField(User, on_delete=models.CASCADE)
     box_address = models.CharField(max_length=100)
+    organization_description = models.CharField(max_length=1000, default='')
 
     def __str__(self):
         return f'{self.organization_id.username } Profile'
